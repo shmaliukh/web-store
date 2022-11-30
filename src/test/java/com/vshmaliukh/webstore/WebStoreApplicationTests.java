db7513a323739755d277be2ac9f5feac76ab766d
vshmaliukh <zskat02@gmail.com>
2022-11-30 14:25:56 +0200

    Created spring boot base application files for future web-store app development

---
+++ src/test/java/com/vshmaliukh/webstore/WebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vshmaliukh.webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
