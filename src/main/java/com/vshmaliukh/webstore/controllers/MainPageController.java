17ced861e314dabb2fbaed3e87aa3aabd51b5234
sharp-n <sof8013@gmail.com>
2022-12-05 16:25:25 +0200

    create block for templates and develop attributes adding

--- src/main/java/com/vshmaliukh/webstore/controllers/MainPageController.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/MainPageController.java
@@ -13,18 +13,23 @@ import org.springframework.web.servlet.ModelAndView;
 public class MainPageController {
 
     @GetMapping
-    public ModelAndView showMainPage(){
-        // todo create main page
-        return new ModelAndView("main-page");
+    public ModelAndView showMainPage(ModelMap modelMap){
+        // todo create main page and draw icons
+        return new ModelAndView("main-page",addPageMainComponents(modelMap));
     }
 
     @GetMapping("/catalog")
     public ModelAndView showCatalogPage(ModelMap modelMap){
-        modelMap.addAttribute("price","$666.00"); // todo create constants and tiles genering for catalog
-        ModelAndView modelAndView = new ModelAndView("catalog",modelMap);
+        modelMap.addAttribute("price","$666.00");// todo create constants and tiles genering for catalog
+        ModelAndView modelAndView = new ModelAndView("catalog",addPageMainComponents(modelMap));
 
         return modelAndView;
     }
 
+    ModelMap addPageMainComponents(ModelMap modelMap){
+        modelMap.addAttribute("header","header"); // todo extract method to any other class
+        modelMap.addAttribute("footer","footer");
+        return modelMap;
+    }
 
 }
