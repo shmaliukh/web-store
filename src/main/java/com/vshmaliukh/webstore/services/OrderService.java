9c1c8b79136a341a0abb5e86acb7ae3735f2960b
vshmaliukh <zskat02@gmail.com>
2022-12-09 15:46:52 +0200

    Added validation for 'order' page Fixed 'spring.jpa.open-in-view' warning Added 'commons-lang3' dependency Added post action for 'order' submit button

--- src/main/java/com/vshmaliukh/webstore/services/OrderService.java
+++ src/main/java/com/vshmaliukh/webstore/services/OrderService.java
@@ -3,8 +3,11 @@ package com.vshmaliukh.webstore.services;
 import com.vshmaliukh.webstore.model.Order;
 import com.vshmaliukh.webstore.model.items.Item;
 import com.vshmaliukh.webstore.repositories.OrderRepository;
+
 import java.util.Collections;
+
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,9 +24,13 @@ public class OrderService {
 
     public void changeOrderStatus(long userId, String newStatusStr) {
         Order orderByUserId = readOrderByUserId(userId);
-        orderByUserId.setStatus(newStatusStr);
-        orderRepository.save(orderByUserId);
-        log.info("userId: '{}' // changed order status, new status: '{}'", userId, newStatusStr);
+        if (orderByUserId != null && StringUtils.isNotBlank(newStatusStr)) {
+            orderByUserId.setStatus(newStatusStr);
+            orderRepository.save(orderByUserId);
+            log.info("userId: '{}' // changed order status, new status: '{}'", userId, newStatusStr);
+        } else {
+            log.warn("userId: '{}' // problem to change order status // order: '{} // status str: '{}'", userId, orderByUserId, newStatusStr);
+        }
     }
 
     public void addItemToOrder(long userId, Item item) {
@@ -46,7 +53,7 @@ public class OrderService {
 
     public List<Item> readOrderItemListByUserId(long userId) {
         Order readOrderByUserId = readOrderByUserId(userId);
-        if(readOrderByUserId != null){
+        if (readOrderByUserId != null) {
             return readOrderByUserId.getItemList();
         }
         log.warn("userId: '{}' // order item list is empty", userId);
