bc306056194bdbf97e16cdaa516f626649e8c570
sharp-n <sof8013@gmail.com>
2022-12-11 23:12:12 +0200

    add template for printing item info in item page method

--- src/main/java/com/vshmaliukh/webstore/controllers/MainPageController.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/MainPageController.java
@@ -1,9 +1,11 @@
 package com.vshmaliukh.webstore.controllers;
 
+import com.vshmaliukh.webstore.model.items.literature_item_imp.Book;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,9 +30,21 @@ public class MainPageController {
         return new ModelAndView(CATALOG_VIEW);
     }
 
-    @GetMapping("/" + CATALOG_PAGE + "/" + CATEGORY_PAGE + "/" + ITEM_PAGE)
-    public ModelAndView showItemPage(ModelMap modelMap){
-        return new ModelAndView(ITEM_PAGE_VIEW); // todo create item-page
+    @GetMapping("/" + CATALOG_PAGE + "/{" + CATEGORY_PAGE + "}/{" + ITEM_PAGE + "}/{id}")
+    public ModelAndView showItemPage(ModelMap modelMap,
+                                     @PathVariable String category,
+                                     @PathVariable String item,
+                                     @PathVariable Long id){ // or Integer - idk
+
+        // getting item from db by item name and id and category name?
+
+        Book book = new Book();
+        String bookInfo = book.getName() + " - " + book.getPrice();   // for catalog tiles
+        String bookDetailInfo = book.getAuthor() + " - " + book.getPages() + " - " + book.getDateOfIssue() + " - "; // info for short description
+        String bookPrice = Integer.toString(book.getPrice()); // separate for price ?
+        String bookName = book.getName();
+
+        return new ModelAndView(ITEM_PAGE_VIEW); // todo create item-page and implement item details printing
     }
 
 }
