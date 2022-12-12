88a3d33e9c1cb444f7b040bb8d67f5d72197dbf5
vshmaliukh <zskat02@gmail.com>
2022-12-12 19:15:59 +0200

    Fixed 'admin-home' page template

--- src/main/java/com/vshmaliukh/webstore/services/ItemService.java
+++ src/main/java/com/vshmaliukh/webstore/services/ItemService.java
@@ -1,10 +1,32 @@
 package com.vshmaliukh.webstore.services;
 
+import com.vshmaliukh.webstore.model.items.Item;
+import com.vshmaliukh.webstore.repositories.ItemRepositoryProvider;
+import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Component
+@AllArgsConstructor
 public class ItemService {
 
+    final ItemRepositoryProvider itemRepositoryProvider;
+
+//    public Integer calcItemsBtCategory(){
+//        Map categoryNameQuantityMap = new HashMap<>();
+//        Collection<JpaRepository<? extends Item, Integer>> jpaRepositories = itemRepositoryProvider.getItemClassTypeRepositoryMap().values();
+//        jpaRepositories.stream()
+//                .map(JpaRepository::findAll)
+//                .flatMap(Collection::stream)
+//                .map(o->categoryNameQuantityMap.put(o.getCategory()))
+//                .collect(Collectors.toList());
+//
+//
+//        return null;
+//    }
 
 
 }
