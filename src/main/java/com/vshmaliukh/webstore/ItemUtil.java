d97d179c0d857dd17854b22ef5663fed0fe11223
vshmaliukh <zskat02@gmail.com>
2022-12-12 11:56:54 +0200

    Created new 'add-item' template Created ItemRepositoryProvider util class

---
+++ src/main/java/com/vshmaliukh/webstore/ItemUtil.java
@@ -0,0 +1,43 @@
+package com.vshmaliukh.webstore;
+
+
+import com.vshmaliukh.webstore.model.items.Item;
+import com.vshmaliukh.webstore.model.items.literature_item_imp.Book;
+import com.vshmaliukh.webstore.model.items.literature_item_imp.Comics;
+import com.vshmaliukh.webstore.model.items.literature_item_imp.Magazine;
+import com.vshmaliukh.webstore.model.items.literature_item_imp.Newspaper;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public final class ItemUtil {
+
+    public static List<String> itemNameList;
+    public static List<String> categoryNameList;
+    public static Map<String, Class<? extends Item>> itemNameClassTypeMap;
+
+    public static final Class<Book> BOOK_CLASS = Book.class;
+    public static final Class<Magazine> MAGAZINE_CLASS = Magazine.class;
+    public static final Class<Comics> COMICS_CLASS = Comics.class;
+    public static final Class<Newspaper> NEWSPAPER_CLASS = Newspaper.class;
+
+    static {
+        generateItemNameClassTypeMap();
+
+        Set<String> keySet = itemNameClassTypeMap.keySet();
+        itemNameList = Collections.unmodifiableList(new ArrayList<>(keySet));
+    }
+
+    private static void generateItemNameClassTypeMap(){
+        Map<String, Class<? extends Item>> nameClassMap = new ConcurrentHashMap<>();
+        nameClassMap.put(BOOK_CLASS.getSimpleName(), BOOK_CLASS);
+        nameClassMap.put(MAGAZINE_CLASS.getSimpleName(), MAGAZINE_CLASS);
+        nameClassMap.put(COMICS_CLASS.getSimpleName(), COMICS_CLASS);
+        nameClassMap.put(NEWSPAPER_CLASS.getSimpleName(), NEWSPAPER_CLASS);
+        itemNameClassTypeMap = Collections.unmodifiableMap(nameClassMap);
+    }
+
+
+
+}
---
+++ src/main/java/com/vshmaliukh/webstore/repositories/ItemRepositoryProvider.java
@@ -0,0 +1,40 @@
+package com.vshmaliukh.webstore.repositories;
+
+import com.vshmaliukh.webstore.model.items.Item;
+import com.vshmaliukh.webstore.model.items.literature_item_imp.Book;
+import com.vshmaliukh.webstore.model.items.literature_item_imp.Comics;
+import com.vshmaliukh.webstore.model.items.literature_item_imp.Magazine;
+import com.vshmaliukh.webstore.model.items.literature_item_imp.Newspaper;
+import com.vshmaliukh.webstore.repositories.literature_items_repositories.BookRepository;
+import com.vshmaliukh.webstore.repositories.literature_items_repositories.ComicsRepository;
+import com.vshmaliukh.webstore.repositories.literature_items_repositories.MagazineRepository;
+import com.vshmaliukh.webstore.repositories.literature_items_repositories.NewspaperRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@AllArgsConstructor
+public final class ItemRepositoryProvider {
+
+    final BookRepository bookRepository;
+    final ComicsRepository comicsRepository;
+    final MagazineRepository magazineRepository;
+    final NewspaperRepository newspaperRepository;
+
+    public final Map<Class<? extends Item>, JpaRepository<? extends Item, Integer>> itemClassTypeRepositoryMap = Collections.unmodifiableMap(generateItemClassTypeRepositoryMap());
+
+    private Map<Class<? extends Item>, JpaRepository<? extends Item, Integer>> generateItemClassTypeRepositoryMap(){
+        Map<Class<? extends Item>, JpaRepository<? extends Item, Integer>> classTypeRepositoryMap = new ConcurrentHashMap<>();
+        classTypeRepositoryMap.put(Book.class, bookRepository);
+        classTypeRepositoryMap.put(Magazine.class, magazineRepository);
+        classTypeRepositoryMap.put(Comics.class, comicsRepository);
+        classTypeRepositoryMap.put(Newspaper.class, newspaperRepository);
+        return classTypeRepositoryMap;
+    }
+
+}
