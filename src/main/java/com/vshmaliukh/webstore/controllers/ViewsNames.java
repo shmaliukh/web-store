30510e32a34fe10b84c6689f98d81f27a4f6b1ef
sharp-n <sof8013@gmail.com>
2022-12-16 11:44:20 +0200

    create users homepage

--- src/main/java/com/vshmaliukh/webstore/controllers/ViewsNames.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/ViewsNames.java
@@ -6,7 +6,7 @@ public class ViewsNames {
     public static final String ITEM_PAGE_VIEW = "item-page";
     public static final String MAIN_PAGE_VIEW = "main-page";
     public static final String SHOPPING_CART_VIEW = "shopping-cart";
-    public static final String HOME_PAGE_VIEW = "home-page";
+    public static final String HOME_PAGE_VIEW = "user-home-page";
     public static final String EDIT_DATA_USERS_VIEW = "edit-data-users-page";
 
 }
