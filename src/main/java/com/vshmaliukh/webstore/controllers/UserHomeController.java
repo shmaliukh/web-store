42c1560e35ef5bf7647199c265f2c78ce249836b
sharp-n <sof8013@gmail.com>
2022-12-16 14:02:28 +0200

    implement controller for users edit page

--- src/main/java/com/vshmaliukh/webstore/controllers/UserHomeController.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/UserHomeController.java
@@ -5,12 +5,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import static com.vshmaliukh.webstore.controllers.ConstantsForControllers.EDIT_PAGE;
 import static com.vshmaliukh.webstore.controllers.ConstantsForControllers.USER_HOME;
-import static com.vshmaliukh.webstore.controllers.ViewsNames.EDIT_DATA_USERS_VIEW;
 import static com.vshmaliukh.webstore.controllers.ViewsNames.HOME_PAGE_VIEW;
 
 @Controller
@@ -27,9 +27,9 @@ public class UserHomeController {
         return new ModelAndView(HOME_PAGE_VIEW,modelMap); // todo create home page
     }
 
-    @GetMapping("/" + EDIT_PAGE)
-    public ModelAndView showUsersEditPage(ModelMap modelMap){
-        return new ModelAndView(EDIT_DATA_USERS_VIEW); // todo create users edit page
+    @GetMapping("/{data}")
+    public ModelAndView showUsersEditPage(@PathVariable String data, ModelMap modelMap){
+        return new ModelAndView(data); // todo create users edit page
     }
 
 }
