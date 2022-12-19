80c36ebb7dbdaf048331d1734c8f96bb59904b0e
vshmaliukh <zskat02@gmail.com>
2022-12-19 10:55:58 +0200

    Hotfix

--- src/main/java/com/vshmaliukh/webstore/model/Order.java
+++ src/main/java/com/vshmaliukh/webstore/model/Order.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 
 import static com.vshmaliukh.webstore.ConstantsForEntities.*;
 
+
 @Getter
 @Setter
 @Entity
@@ -44,7 +45,7 @@ public class Order {
     @Column(name = ITEM_ID_COLUMN, nullable = false)
     private Integer itemId;
 
-    @Column(name = ITEM_ID_COLUMN, nullable = false)
+    @Column(name = ITEM_CLASS_TYPE_COLUMN, nullable = false)
     private Class<Item> itemClassType;
 
 //    @OneToMany
