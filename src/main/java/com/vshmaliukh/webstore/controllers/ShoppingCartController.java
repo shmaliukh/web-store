c7f81a9edbfacfdcfbccc307da9e5820b4dcc74e
sharp-n <sof8013@gmail.com>
2022-12-26 08:09:56 +0200

    add test carts to cart controller

--- src/main/java/com/vshmaliukh/webstore/controllers/ShoppingCartController.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/ShoppingCartController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.vshmaliukh.webstore.controllers.ConstantsForControllers.SHOPPING_CART;
@@ -32,7 +33,8 @@ public class ShoppingCartController {
 
     @GetMapping
     public ModelAndView showCartPage(ModelMap modelMap){
-        List<Cart> carts = cartService.getCartsByUserId(userService.readUserIdByName("")); // todo add username usage
+//        List<Cart> carts = cartService.getCartsByUserId(userService.readUserIdByName("")); // todo add username usage
+        List<Cart> carts = getTestCarts();
         modelMap.addAttribute("items",carts);
         modelMap.addAttribute("totalItems",carts.size()+1);
         int totalPrice = 0;
@@ -60,6 +62,27 @@ public class ShoppingCartController {
         Item item = itemRepositoryProvider.getActionsWithItemRepositoryByItemClassName(type).getItemById(id);
         cartService.decItemQuantityInCart(item,"username"); // todo implement username usage
         return "redirect:/" + SHOPPING_CART;
+    }
+
+    public List<Cart> getTestCarts(){
+        List<Cart> carts = new ArrayList<>();
+
+        Cart first = new Cart();
+        first.setCartId(Long.getLong("1"));
+        first.setUserId(Long.getLong("1"));
+        first.setCategory("book");
+        first.setItemQuantity(4);
+        first.setPrice(1000);
+
+        Cart second = new Cart();
+        second.setCartId(Long.getLong("1"));
+        second.setUserId(Long.getLong("1"));
+        second.setCategory("book");
+        second.setItemQuantity(4);
+        second.setPrice(1000);
+        carts.add(first);
+        carts.add(second);
+        return carts;
     }
 
 }
