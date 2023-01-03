d477f1683d6d9a47016b3c084d27683a48aa6995
sharp-n <sof8013@gmail.com>
2023-01-03 10:21:49 +0200

    create method for checking existence in UnauthorizedUserRepository

--- src/main/java/com/vshmaliukh/webstore/repositories/UnauthorizedUserRepository.java
+++ src/main/java/com/vshmaliukh/webstore/repositories/UnauthorizedUserRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UnauthorizedUserRepository extends JpaRepository<UnauthorizedUser, Long> {
 
-
+    boolean existsById(Long id);
 
 }
