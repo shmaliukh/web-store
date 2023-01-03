a69f86ab4902af015c29cc488e0b717ad06a1fd0
sharp-n <sof8013@gmail.com>
2023-01-03 09:57:44 +0200

    create repository for unauthorized users

---
+++ src/main/java/com/vshmaliukh/webstore/repositories/UnauthorizedUserRepository.java
@@ -0,0 +1,13 @@
+package com.vshmaliukh.webstore.repositories;
+
+import com.vshmaliukh.webstore.model.UnauthorizedUser;
+import com.vshmaliukh.webstore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnauthorizedUserRepository extends JpaRepository<UnauthorizedUser, Long> {
+
+
+
+}
