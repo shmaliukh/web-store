1a64bc0bb84f792bf1ef038e9da0d8466d34514d
sharp-n <sof8013@gmail.com>
2023-01-03 14:12:41 +0200

    create error controller class

---
+++ src/main/java/com/vshmaliukh/webstore/controllers/StoreErrorController.java
@@ -0,0 +1,10 @@
+package com.vshmaliukh.webstore.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error")
+public class StoreErrorController {
+
+}
