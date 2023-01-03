7f8094917e55cdafcfa8b73c73e800f1f0dbf5b5
sharp-n <sof8013@gmail.com>
2023-01-03 10:01:35 +0200

    create service for unauthorized users

---
+++ src/main/java/com/vshmaliukh/webstore/services/UnauthorizedUserService.java
@@ -0,0 +1,16 @@
+package com.vshmaliukh.webstore.services;
+
+import com.vshmaliukh.webstore.model.UnauthorizedUser;
+import com.vshmaliukh.webstore.repositories.UnauthorizedUserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class UnauthorizedUserService {
+
+    final UnauthorizedUserRepository unauthorizedUserRepository;
+
+}
