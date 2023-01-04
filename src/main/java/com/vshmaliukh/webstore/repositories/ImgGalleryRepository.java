6a0c844ba130a07b8510cd947355f495d67f0111
vshmaliukh <zskat02@gmail.com>
2023-01-04 08:41:42 +0200

    Created image gallery base entity and repository classes

---
+++ src/main/java/com/vshmaliukh/webstore/repositories/ImgGalleryRepository.java
@@ -0,0 +1,7 @@
+package com.vshmaliukh.webstore.repositories;
+
+import com.vshmaliukh.webstore.model.ImgGallery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImgGalleryRepository extends JpaRepository<ImgGallery, Long> {
+}
