9b6ff896e3359b39f0d2f079902e2f3d1193b27e
sharp-n <sof8013@gmail.com>
2023-01-05 14:46:12 +0200

    add logging to exception handler class

--- src/main/java/com/vshmaliukh/webstore/controllers/handlers/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/handlers/RestResponseEntityExceptionHandler.java
@@ -1,23 +1,21 @@
 package com.vshmaliukh.webstore.controllers.handlers;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.ModelMap;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler
         extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value
             = { IllegalArgumentException.class, IllegalStateException.class })
-    protected ModelAndView handleRuntimeException() {
+    protected ModelAndView handleRuntimeException(Exception exception) {
+        log.error(exception.getMessage());
         return new ModelAndView("error");
     }
 
