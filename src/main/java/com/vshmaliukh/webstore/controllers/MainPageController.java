33b4aada0dfb48d847978ebcafda246fab6cea9c
sharp-n <sof8013@gmail.com>
2023-01-06 11:02:08 +0200

    refactor item getting from db in main page controller

--- src/main/java/com/vshmaliukh/webstore/controllers/MainPageController.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/MainPageController.java
@@ -2,12 +2,8 @@ package com.vshmaliukh.webstore.controllers;
 
 import com.vshmaliukh.webstore.controllers.handlers.CookieHandler;
 import com.vshmaliukh.webstore.model.items.Item;
-import com.vshmaliukh.webstore.model.items.literature_item_imp.Book;
-import com.vshmaliukh.webstore.model.items.literature_item_imp.Comics;
-import com.vshmaliukh.webstore.model.items.literature_item_imp.Magazine;
 import com.vshmaliukh.webstore.repositories.ItemRepositoryProvider;
 import com.vshmaliukh.webstore.repositories.literature_items_repositories.BaseItemRepository;
-import com.vshmaliukh.webstore.repositories.literature_items_repositories.BookRepository;
 import com.vshmaliukh.webstore.services.CartService;
 import com.vshmaliukh.webstore.services.ItemService;
 import com.vshmaliukh.webstore.services.UnauthorizedUserService;
@@ -19,7 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/main")
@@ -36,15 +34,15 @@ public class MainPageController {
     public ModelAndView showMainPage(ModelMap modelMap) {
         // todo refactor template for links
         List<String> types = new ArrayList<>();
-        itemRepositoryProvider.itemClassNameRepositoryMap.forEach((k,o)->types.add(k.toUpperCase()));
+        itemRepositoryProvider.itemClassNameRepositoryMap.forEach((k, o) -> types.add(k.toUpperCase()));
 //        modelMap.addAttribute("categories", categories);
         modelMap.addAttribute("types", types);
-        return new ModelAndView("main-page",modelMap);
+        return new ModelAndView("main-page", modelMap);
     }
 
     @GetMapping("/catalog/{type}")
     public ModelAndView showCatalogPage(ModelMap modelMap,
-                                  @PathVariable String type) {
+                                        @PathVariable String type) {
         List<? extends Item> items = itemService.readAllItemsByTypeName(type);
         modelMap.addAttribute("itemList", items);
         return new ModelAndView("catalog");
@@ -52,11 +50,15 @@ public class MainPageController {
 
     @GetMapping("/catalog/{type}/{id}")
     public ModelAndView showItemPage(ModelMap modelMap,
-                               @PathVariable String type,
-                               @PathVariable Integer id) {
-        Item item = itemRepositoryProvider.getItemRepositoryByItemClassName(type).findById(id).get();
+                                     @PathVariable String type,
+                                     @PathVariable Integer id) {
+        Item item = null;
+        Optional<? extends Item> optionalItem = itemRepositoryProvider.getItemRepositoryByItemClassName(type).findById(id);
+        if (optionalItem.isPresent()) {
+            item = optionalItem.get();
+        }
         modelMap.addAttribute("type", type.toLowerCase());
-        modelMap.addAttribute("item",item);
+        modelMap.addAttribute("item", item);
         return new ModelAndView("item-page");
     }
 
