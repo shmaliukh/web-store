9ce5bba9605d216c1ce010cdd032e84203876b15
sharp-n <sof8013@gmail.com>
2023-01-09 08:41:47 +0200

    remove test methods

--- src/main/java/com/vshmaliukh/webstore/controllers/ShoppingCartController.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/ShoppingCartController.java
@@ -3,9 +3,6 @@ package com.vshmaliukh.webstore.controllers;
 import com.vshmaliukh.webstore.controllers.handlers.CookieHandler;
 import com.vshmaliukh.webstore.model.Cart;
 import com.vshmaliukh.webstore.model.items.Item;
-import com.vshmaliukh.webstore.model.items.literature_item_imp.Book;
-import com.vshmaliukh.webstore.model.items.literature_item_imp.Comics;
-import com.vshmaliukh.webstore.model.items.literature_item_imp.Magazine;
 import com.vshmaliukh.webstore.repositories.ItemRepositoryProvider;
 import com.vshmaliukh.webstore.services.CartService;
 import com.vshmaliukh.webstore.services.ItemService;
@@ -19,7 +16,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,43 +36,27 @@ public class ShoppingCartController {
     public ModelAndView showCartPage(ModelMap modelMap,
                                      HttpServletResponse response,
                                      @CookieValue(required = false, defaultValue = "0") Long userId) {
-        List<Item> testItems = getTestItemOrderList(); // for tests
         if (userId == 0) {
             userId = unauthorizedUserService.createUnauthorizedUser().getId();
             response.addCookie(cookieHandler.createUserIdCookie(userId));
         }
-
-//        List<Cart> carts = cartService.getCartsByUserId(userService.getUserById(id).getId()); // todo uncomment when test items will be removed
-//        List<Item> items = new ArrayList<>();
-//        for (Cart cart : carts) {
-//            Item item = itemRepositoryProvider.getItemRepositoryByItemClassName(cart.getCategory())
-//                    .getById(cart.getItemId());
-//            item.setPrice(item.getPrice()*item.getQuantity());
-//            items.add(item);
-//        }
-
-        for (Item item : testItems) { // for tests
-            item.setPrice(item.getPrice() * item.getQuantity());
+        List<Cart> carts = cartService.getCartsByUserId(userService.getUserById(userId).getId()); // todo uncomment when test items will be removed
+        List<Item> items = new ArrayList<>();
+        for (Cart cart : carts) {
+            Item item = itemRepositoryProvider.getItemRepositoryByItemClassName(cart.getCategory())
+                    .getById(cart.getItemId());
+            item.setPrice(item.getPrice()*item.getQuantity());
+            items.add(item);
         }
-
         int totalCount = 0;
         int totalPrice = 0;
-
-//        for (Item item : items) {
-//            totalPrice = totalPrice + item.getPrice();
-//        }
-//        for (Item item : items) {
-//            totalCount = totalCount + item.getQuantity();
-//        }
-
-        for (Item item : testItems) {  // for tests
+        for (Item item : items) {
             totalPrice = totalPrice + item.getPrice();
         }
-        for (Item item : testItems) { // for tests
+        for (Item item : items) {
             totalCount = totalCount + item.getQuantity();
         }
-
-        modelMap.addAttribute("items", testItems);
+        modelMap.addAttribute("items", items);
         modelMap.addAttribute("totalItems", totalCount);
         modelMap.addAttribute("totalPrice", totalPrice);
         return new ModelAndView("shopping-cart");
@@ -99,36 +79,6 @@ public class ShoppingCartController {
         Optional<? extends Item> optionalItem = itemRepositoryProvider.getItemRepositoryByItemClassName(type).findById(id);
         optionalItem.ifPresent(item -> cartService.decItemQuantityInCart(item, userId));
         return "redirect:/shopping-cart";
-    }
-
-    public List<Cart> getTestCarts() {
-        List<Cart> carts = new ArrayList<>();
-
-        Cart first = new Cart();
-        first.setCartId(Long.getLong("1"));
-        first.setUserId(Long.getLong("1"));
-        first.setCategory("book");
-        first.setItemQuantity(4);
-        first.setPrice(1000);
-
-        Cart second = new Cart();
-        second.setCartId(Long.getLong("1"));
-        second.setUserId(Long.getLong("1"));
-        second.setCategory("book");
-        second.setItemQuantity(4);
-        second.setPrice(1000);
-        carts.add(first);
-        carts.add(second);
-        return carts;
-    }
-
-    private static List<Item> getTestItemOrderList() {
-        List<Item> itemList = new ArrayList<>();
-        itemList.add(new Book(1, "1 book name", "book", 2, 3, true, 4, "Vlad1", new Date()));
-        itemList.add(new Book(2, "2 book name", "book", 3, 4, true, 5, "Vlad2", new Date()));
-        itemList.add(new Magazine(3, "Magazine name", "magazine", 4, 5, true, 6));
-        itemList.add(new Comics(4, "Comics name", "comics", 5, 6, true, 7, "Some publisher"));
-        return itemList;
     }
 
 }
