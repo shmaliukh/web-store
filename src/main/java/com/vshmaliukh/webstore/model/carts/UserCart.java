90de2268fe1dca4b97d32162dcfdfb0538334eb6
sharp-n <sof8013@gmail.com>
2023-01-10 09:23:55 +0200

    add users cart extended from cart entity

---
+++ src/main/java/com/vshmaliukh/webstore/model/carts/UserCart.java
@@ -0,0 +1,26 @@
+package com.vshmaliukh.webstore.model.carts;
+
+import com.vshmaliukh.webstore.model.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import static com.vshmaliukh.webstore.ConstantsForEntities.USER_COLUMN;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "User_carts")
+public class UserCart extends Cart {
+
+    @ManyToOne
+    @Column(name = USER_COLUMN, nullable = false)
+    private User user;
+
+}
