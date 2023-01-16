393b475bf433e6bfacbe4b5d7e0a5c18b2780849
YANA <sof8013@gmail.com>
2023-01-16 11:36:30 +0200

    implement CartItem entity

---
+++ src/main/java/com/vshmaliukh/webstore/model/items/CartItem.java
@@ -0,0 +1,42 @@
+package com.vshmaliukh.webstore.model.items;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+import static com.vshmaliukh.webstore.ConstantsForEntities.ORDER_ITEM_TABLE;
+
+@Slf4j
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "Cart_item")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_item_id", nullable = false)
+    private Long id;
+
+    @OneToOne
+    private Item item;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+    public void setQuantity(int quantity) {
+
+        // todo mb implement quantity check
+
+        if(quantity>0){
+            this.quantity = quantity;
+        } else{
+            this.quantity = 1;
+        }
+
+    }
+}
