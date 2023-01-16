5840b205530fc47b35430d112e8fd46af1015513
vshmaliukh <zskat02@gmail.com>
2023-01-16 10:47:58 +0200

    Created base category 'catalog' template Implemented base category controller Added 'category panel' button to 'admin-menu' template

--- src/main/java/com/vshmaliukh/webstore/controllers/admin/AdminCategoryController.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/admin/AdminCategoryController.java
@@ -1,5 +1,6 @@
 package com.vshmaliukh.webstore.controllers.admin;
 
+import com.vshmaliukh.webstore.model.Category;
 import com.vshmaliukh.webstore.services.CategoryService;
 import com.vshmaliukh.webstore.services.ItemService;
 import lombok.AllArgsConstructor;
@@ -7,8 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -20,8 +25,24 @@ public class AdminCategoryController {
     final CategoryService categoryService;
 
     @GetMapping("/catalog")
-    public ModelAndView doGetCatalog(ModelMap modelMap){
+    public ModelAndView doGetCatalog(ModelMap modelMap) {
+        List<Category> categoryList = categoryService.readAll();
+        
+        modelMap.addAttribute("categoryList", categoryList);
         return new ModelAndView("admin/category/catalog", modelMap);
+    }
+
+    @GetMapping("create")
+    public ModelAndView doGetCreate(ModelMap modelMap) {
+        return new ModelAndView("admin/category/create", modelMap);
+    }
+
+    @PostMapping
+    public ModelAndView doPostCreate(@RequestParam(name = "name") 
+
+            ModelMap modelMap) {
+        Category category;
+        return new ModelAndView("redirect:admin/category/details/" + category.getId(), modelMap);
     }
 
 }
