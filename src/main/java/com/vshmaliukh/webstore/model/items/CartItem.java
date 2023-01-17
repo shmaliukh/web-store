0e3d6c0c2b32c57a1aad8dee1968329bde65e6e5
YANA <sof8013@gmail.com>
2023-01-17 09:41:52 +0200

    add setters for fields of cart item

--- src/main/java/com/vshmaliukh/webstore/model/items/CartItem.java
+++ src/main/java/com/vshmaliukh/webstore/model/items/CartItem.java
@@ -2,23 +2,19 @@ package com.vshmaliukh.webstore.model.items;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
 
-import static com.vshmaliukh.webstore.ConstantsForEntities.ORDER_ITEM_TABLE;
-
 @Slf4j
 @Getter
-@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "Cart_item")
 public class CartItem {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cart_item_id", nullable = false)
     private Integer id;
 
@@ -30,6 +26,12 @@ public class CartItem {
 
     @Column(name = "price", nullable = false)
     private int price; // todo change type
+
+    public void setItem(Item item){
+        this.item = item;
+        this.id = item.getId();
+        this.price = item.getSalePrice();
+    }
 
     public void setQuantity(int quantity) {
 
