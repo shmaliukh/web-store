b4880be953023c18d2307b04310ff932cfa8d61c
vshmaliukh <zskat02@gmail.com>
2023-01-30 17:39:18 +0200

    Added 'validation' annotations for 'user' entity

--- src/main/java/com/vshmaliukh/webstore/model/User.java
+++ src/main/java/com/vshmaliukh/webstore/model/User.java
@@ -5,6 +5,7 @@ import com.vshmaliukh.webstore.login.UserRole;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Getter
 @Setter
@@ -17,23 +18,30 @@ public class User extends AuditModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Min(value = 1 , message = "Please provide a valid 'user_id' (min = 1)")
     @Column(name = "user_id")
     private Long id;
 
+    @Size(min = 3, max = 50, message = "Please provide a valid 'username' (min = 3, max = 50)")
     @Column(unique = true)
     private String username;
 
+    @Email(message = "Please provide a valid 'email'")
     @Column(unique = true, nullable = false)
     private String email;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "log_in_provider")
+    @NotEmpty(message = "'role' cannot be empty")
+    @Column(name = "log_in_provider", nullable = false)
     private LogInProvider logInProvider;
 
     @Enumerated(EnumType.STRING)
+    @NotEmpty(message = "'role' cannot be empty")
+    @Column(nullable = false)
     private UserRole role;
 
     @ToString.Exclude
+    @Size(min = 4, max = 50, message = "Please provide a valid 'password' (min = 4, max = 50)")
     private String password;
 
     private boolean enabled;
