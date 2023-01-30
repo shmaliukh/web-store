6e0d6ebbd9d670858517dc8ed5a8d5360343b844
YANA <sof8013@gmail.com>
2023-01-30 09:48:46 +0200

    add methods to cart item repository

--- src/main/java/com/vshmaliukh/webstore/repositories/CartItemRepository.java
+++ src/main/java/com/vshmaliukh/webstore/repositories/CartItemRepository.java
@@ -1,7 +1,13 @@
 package com.vshmaliukh.webstore.repositories;
 
 import com.vshmaliukh.webstore.model.items.CartItem;
+import com.vshmaliukh.webstore.model.items.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Integer> {
+
+    boolean existsByItem(Item item);
+
+    CartItem getCartItemByItem(Item item);
+
 }
