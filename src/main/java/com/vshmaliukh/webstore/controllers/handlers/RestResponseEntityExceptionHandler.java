a6d7d455cc938d1c63a6f58142165283d6feac05
vshmaliukh <zskat02@gmail.com>
2023-02-10 12:16:05 +0200

    Created method to handle not valid exception

--- src/main/java/com/vshmaliukh/webstore/controllers/handlers/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/handlers/RestResponseEntityExceptionHandler.java
@@ -1,10 +1,22 @@
 package com.vshmaliukh.webstore.controllers.handlers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
@@ -18,6 +30,24 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         // TODO exclude '/admin/**'
         log.error(exception.getMessage(), exception);
         return new ModelAndView("error");
+    }
+
+    // error handle for @Valid
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        List<String> errorList = exception.getBindingResult()
+                .getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        body.put("errors", errorList);
+        return new ResponseEntity<>(body, headers, status);
     }
 
 }
