95eda309860b6987e4a522b58583ce2fbbfc8f90
YANA <sof8013@gmail.com>
2023-02-14 09:38:15 +0200

    implement method for getting options for sorting

--- src/main/java/com/vshmaliukh/webstore/controllers/rest/ItemController.java
+++ src/main/java/com/vshmaliukh/webstore/controllers/rest/ItemController.java
@@ -6,19 +6,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 @Slf4j
 @RestController
 @AllArgsConstructor
+@RequestMapping("/item")
 public class ItemController {
 
     final ItemService itemService;
 
-    @GetMapping("/item/status-names")
+    @GetMapping("/status-names")
     public ResponseEntity<List<String>> readItemStatusNameList(){
         List<String> statusSet = itemService.readStatusNameList();
         return ResponseEntity.ok()
@@ -26,12 +29,20 @@ public class ItemController {
                 .body(statusSet);
     }
 
-    @GetMapping("/item/type-names")
+    @GetMapping("/type-names")
     public ResponseEntity<Set<String>> readItemTypeNameSet(){
         Set<String> itemTypeNameSet = itemService.readTypeNameSet();
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(itemTypeNameSet);
+    }
+
+    @GetMapping("/sorting-options")
+    public ResponseEntity<List<String>> readItemOptions(){
+        List<String> sortingOptions = itemService.readSortingOptionsList();
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(sortingOptions);
     }
 
 }
